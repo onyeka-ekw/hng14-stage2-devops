8b2926e697336744ad4e1964cbaf6f171c21bfcf
Symbiotik <ekwunifechris7@gmail.com>
2026-04-22 13:06:19 +0100

    fixes: fix cd/cd

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,18 +3,23 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'commonjs',
   },
   rules: {
     'no-unused-vars': 'error',
     'no-console': 'off',
     'prefer-const': 'error',
     'no-var': 'error',
+  },
+  globals: {
+    'process': 'readonly',
+    '__dirname': 'readonly',
   },
 };
